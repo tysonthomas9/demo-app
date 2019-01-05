98317af7afb91c7026ad8f631ac71b9f45ba812f
Tyson Thomas <tysonthomas@tysons-mbp.home>
2019-01-05 12:17:05 -0500

    Changed the path of static files

---
+++ precache-manifest.a5a3ec5691d78d77b7c7f04abe74e0b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c093073f4cedd280ea45",
+    "url": "../demo-app/static/css/main.f0f39867.chunk.css"
+  },
+  {
+    "revision": "c093073f4cedd280ea45",
+    "url": "../demo-app/static/js/main.c093073f.chunk.js"
+  },
+  {
+    "revision": "e4408353efad2110a549",
+    "url": "../demo-app/static/js/1.e4408353.chunk.js"
+  },
+  {
+    "revision": "b7fa8b5d89ca3c12cfb8",
+    "url": "../demo-app/static/js/runtime~main.b7fa8b5d.js"
+  },
+  {
+    "revision": "7e549b2c8f428625dc1918124ed4845a",
+    "url": "../demo-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.00d68909b09805f18d92122447cf12bb.js"
+  "../demo-app/precache-manifest.a5a3ec5691d78d77b7c7f04abe74e0b4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("../demo-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
